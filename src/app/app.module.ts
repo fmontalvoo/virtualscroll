376ec77f504e88293a27fdb49fc8e201627fc612
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-12 10:37:00 -0500

    Drag basico.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import { AppComponent } from './app.component';
 import { VirtualComponent } from './components/virtual/virtual.component';
+import { DragComponent } from './components/drag/drag.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    DragDropModule,
     ScrollingModule,
   ],
   declarations: [
     AppComponent,
-    VirtualComponent
+    VirtualComponent,
+    DragComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
